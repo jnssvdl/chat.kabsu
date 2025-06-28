d3c0156a2b83e2241780f074bcd90dd34bbf4045
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-06-28 22:43:29 +0800

    fix: validate JWT expiration

--- client/src/hooks/use-auth.ts
+++ client/src/hooks/use-auth.ts
@@ -1,7 +1,26 @@
+import { jwtDecode } from "jwt-decode";
+
 export default function useAuth() {
   const token = localStorage.getItem("jwt");
 
-  const isAuthenticated = !!token;
+  let isAuthenticated = false;
+
+  if (token) {
+    try {
+      const { exp } = jwtDecode(token);
+      const now = Math.floor(Date.now() / 1000);
+
+      if (!exp) return;
+
+      if (exp > now) {
+        isAuthenticated = true;
+      } else {
+        localStorage.removeItem("jwt");
+      }
+    } catch {
+      localStorage.removeItem("jwt");
+    }
+  }
 
   const logout = () => {
     localStorage.removeItem("jwt");
