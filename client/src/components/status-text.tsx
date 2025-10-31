50e06bef84e8eca54f66b304cd479595b096fd5f
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-10-31 20:04:09 +0800

    make status text a reusable component

---
+++ client/src/components/status-text.tsx
@@ -0,0 +1,24 @@
+import type { LucideIcon } from "lucide-react";
+
+type StatusTextProps = {
+  Icon: LucideIcon;
+  text: string;
+  className?: string;
+  iconClassName?: string;
+};
+
+export default function StatusText({
+  Icon,
+  text,
+  className,
+  iconClassName,
+}: StatusTextProps) {
+  return (
+    <p
+      className={`text-muted-foreground flex items-center justify-center gap-2 ${className}`}
+    >
+      <Icon className={`h-4 w-4 ${iconClassName}`} />
+      <span>{text}</span>
+    </p>
+  );
+}
