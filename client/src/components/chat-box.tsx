db643abf17e056bee6d161c17bbc4d51245a6130
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-11-01 19:53:21 +0800

    use gap-2 instead of space-y-2 class

--- client/src/components/chat-box.tsx
+++ client/src/components/chat-box.tsx
@@ -3,7 +3,7 @@ import ChatBubble from "./chat-bubble";
 
 export default function ChatBox({ messages }: { messages: Message[] }) {
   return (
-    <div className="space-y-2">
+    <div className="flex flex-col gap-2">
       {messages.map((message, index) => (
         <ChatBubble key={index} message={message} />
       ))}
