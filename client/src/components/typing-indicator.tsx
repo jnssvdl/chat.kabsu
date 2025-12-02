270fae85f50454ec1bcbb5b6d3c92ac802a8105a
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-12-02 18:53:22 +0800

    fix chat bubble and typing indicator bg and text color for light mode

--- client/src/components/typing-indicator.tsx
+++ client/src/components/typing-indicator.tsx
@@ -1,9 +1,9 @@
 export default function TypingIndicator() {
   return (
-    <div className="bg-card inline-flex max-w-fit items-center space-x-1 rounded-full p-4">
-      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
-      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
-      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
+    <div className="bg-accent inline-flex max-w-fit items-center space-x-1 rounded-full p-4">
+      <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
+      <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
+      <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
     </div>
   );
 }
