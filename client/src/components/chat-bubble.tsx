3c19fa7ff3085a213d3e0e5036c6800257c3ed08
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-12-19 19:26:28 +0800

    update message type and usage in chat bubble

--- client/src/components/chat-bubble.tsx
+++ client/src/components/chat-bubble.tsx
@@ -1,25 +1,22 @@
-import { socket } from "@/lib/socket";
 import type { Message } from "@/types/message";
 
 export default function ChatBubble({ message }: { message: Message }) {
-  const isMe = message.from === socket.id;
-
   return (
     <div
       className={`animate-in flex duration-200 ${
-        isMe
+        message.fromMe
           ? "slide-in-from-bottom-20 justify-end"
           : "slide-in-from-top-20 justify-start"
       }`}
     >
       <div
         className={`max-w-[80%] rounded-2xl px-4 py-2 break-words ${
-          isMe
+          message.fromMe
             ? "bg-primary text-primary-foreground rounded-br-none"
             : "bg-accent text-accent-foreground rounded-tl-none"
         }`}
       >
-        {message.message}
+        {message.text}
       </div>
     </div>
   );
--- client/src/types/message.ts
+++ client/src/types/message.ts
@@ -1,6 +1,4 @@
-import type { Socket } from "socket.io-client";
-
 export type Message = {
-  from: Socket["id"];
-  message: string;
+  fromMe: boolean;
+  text: string;
 };
